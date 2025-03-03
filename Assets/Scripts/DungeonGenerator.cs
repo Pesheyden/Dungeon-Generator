b50f52576dd8b9c8ee539b80e671b737cd33bbdb
Pesheyden <pasha.dan.dof@gmail.com>
2025-03-03 23:30:59 +0100

    Moved nodes to a node system, Added wall extruction

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using NaughtyAttributes;
@@ -9,59 +10,100 @@ public class DungeonGenerator : MonoBehaviour
     [SerializeField] private Vector2Int _minimalRoomSize;
     [SerializeField] private int _wallWidth;
 
+    public RoomNode RootNode;
+
     private List<RectInt> _debugRooms = new();
+    private List<RectInt> _debugWalls = new();
+
+    private void Awake()
+    {
+        //Create root node
+
+        RootNode = new RoomNode(_dungeon);
+    }
 
     private void Start()
     {
-        StartCoroutine(TraverseSpilt(_dungeon, false));
+        StartCoroutine(TraverseSpilt(RootNode, false));
     }
 
-    private IEnumerator TraverseSpilt(RectInt rawRoom, bool doHorizontalSplit)
+    private IEnumerator TraverseSpilt(RoomNode startRoom, bool doHorizontalSplit)
     {
         RectInt newRoom1;
         RectInt newRoom2;
 
+        //Create new raw rooms
         if (doHorizontalSplit)
         {
             newRoom1 = new RectInt(
-                rawRoom.x,
-                rawRoom.y,
-                rawRoom.width,
-                rawRoom.height / 2  + _wallWidth / 2);
+                startRoom.RoomValue.x,
+                startRoom.RoomValue.y,
+                startRoom.RoomValue.width,
+                startRoom.RoomValue.height / 2  + _wallWidth / 2);
 
             newRoom2 = new RectInt(
-                rawRoom.x,
-                rawRoom.y + rawRoom.height / 2 - _wallWidth / 2,
-                rawRoom.width,
-                rawRoom.height / 2 + _wallWidth / 2);
+                startRoom.RoomValue.x,
+                startRoom.RoomValue.y + startRoom.RoomValue.height / 2 - _wallWidth / 2,
+                startRoom.RoomValue.width,
+                startRoom.RoomValue.height / 2 + _wallWidth / 2);
         }
         else
         {
             newRoom1 = new RectInt(
-                rawRoom.x,
-                rawRoom.y,
-                rawRoom.width / 2  + _wallWidth / 2,
-                rawRoom.height);
+                startRoom.RoomValue.x,
+                startRoom.RoomValue.y,
+                startRoom.RoomValue.width / 2  + _wallWidth / 2,
+                startRoom.RoomValue.height);
 
             newRoom2 = new RectInt(
-                rawRoom.x + rawRoom.width / 2 - _wallWidth / 2,
-                rawRoom.y,
-                rawRoom.width / 2 + _wallWidth / 2,
-                rawRoom.height);
+                startRoom.RoomValue.x + startRoom.RoomValue.width / 2 - _wallWidth / 2,
+                startRoom.RoomValue.y,
+                startRoom.RoomValue.width / 2 + _wallWidth / 2,
+                startRoom.RoomValue.height);
         }
 
+        //If new rooms are too small - break
         if (newRoom1.width < _minimalRoomSize.x || newRoom1.height < _minimalRoomSize.y)
             yield break;
         if(newRoom2.width < _minimalRoomSize.x || newRoom2.height < _minimalRoomSize.y)
             yield break;
         
+        //Extract wall
+        RectInt newWall = AlgorithmsUtils.Intersect(newRoom1, newRoom2);
         
+        //Remove wall from rooms
+        if (doHorizontalSplit)
+        {
+            newRoom1.height -= _wallWidth;
+            newRoom2.height -= _wallWidth;
+            newRoom2.y += _wallWidth;
+        }
+        else
+        {
+            newRoom1.width -= _wallWidth;
+            newRoom2.width -= _wallWidth;
+            newRoom2.x += _wallWidth;
+        }
+        
+        //Debug
         _debugRooms.Add(newRoom1);
         _debugRooms.Add(newRoom2);
-        Debug.Log(newRoom1 + " " + newRoom2);
+        _debugWalls.Add(newWall);
+        Debug.Log("Rooms:" + newRoom1 + ":!:" + newRoom2 + "Wall: " + newWall);
+        
+        //Filling nodes
+
+        RoomNode roomNode1 = new RoomNode(newRoom1);
+        RoomNode roomNode2 = new RoomNode(newRoom2);
+
+        startRoom.Room1 = roomNode1;
+        startRoom.Room2 = roomNode2;
+        startRoom.WallValue = newWall;
+        
+        
         yield return new WaitForSeconds(1f);
-        StartCoroutine(TraverseSpilt(newRoom1, !doHorizontalSplit));
-        StartCoroutine(TraverseSpilt(newRoom2, !doHorizontalSplit));
+        StartCoroutine(TraverseSpilt(roomNode1, !doHorizontalSplit));
+        StartCoroutine(TraverseSpilt(roomNode2, !doHorizontalSplit));
 
     }
 
@@ -71,6 +113,10 @@ public class DungeonGenerator : MonoBehaviour
         {
             AlgorithmsUtils.DebugRectInt(room, Color.yellow);
         }
+        foreach (var wall in _debugWalls)
+        {
+            AlgorithmsUtils.DebugRectInt(wall, Color.blue);
+        }
 
         AlgorithmsUtils.DebugRectInt(_dungeon, Color.red);
     }
@@ -78,7 +124,13 @@ public class DungeonGenerator : MonoBehaviour
 
 public class RoomNode
 {
+    public RoomNode(RectInt roomValue)
+    {
+        RoomValue = roomValue;
+    }
+
     public RectInt RoomValue;
+    public RectInt WallValue;
     public RoomNode Room1;
     public RoomNode Room2;
 }
