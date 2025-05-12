af9b1154c527905f9d9389db3a778375695e3608
Pesheyden <pasha.dan.dof@gmail.com>
2025-05-12 09:43:41 +0200

    Added wall creation and debug await customization

--- Assets/Scripts/DungeonFiller.cs
+++ Assets/Scripts/DungeonFiller.cs
@@ -5,10 +5,15 @@ using DungeonGeneration.Graph;
 using NaughtyAttributes;
 using UnityEngine;
 
+public enum WallDebugMode {EachTile,Line,None }
 public class DungeonFiller : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject[] _tilesPrefabs;
+    [SerializeField] private GameObject[] _debugPrefabs;
+    [SerializeField] private int _floodFillAwaitTime = 1; 
+    [SerializeField] private int _wallCreationAwaitTime = 1;
+    [SerializeField] private WallDebugMode _wallDebugMode;
+    
+    [SerializeField] private GameObject[] _tilesPrefabs;
     private Graph _dungeonGraph;
     private List<Transform> _dungeonRoomsTransforms = new List<Transform>();
 
@@ -47,15 +52,16 @@ public class DungeonFiller : MonoBehaviour
             AlgorithmsUtils.FillRectangle(_dungeonTileMap, door.Dimensions, 0);
         }
 
-        DebugTileMap(true);
+        DebugTileMap(true,false);
     }
     [Button]
     public async void FillTheDungeon()
     {
         CreateRoomsTransforms();
         await FloodFillFloor();
+        await SpawnWalls();
     }
-    
+
     private void CreateRoomsTransforms()
     {
         foreach (var room in _dungeonGraph.GetRooms())
@@ -94,15 +100,62 @@ public class DungeonFiller : MonoBehaviour
         floor.transform.parent = _dungeonRoomsTransforms[_dungeonRoomsMap[(int)point.z, (int)point.x]];
         floor.name = $"Floor_{point.x},{point.z}";
 
-        await Task.Delay(1);
-        Fill(new Vector3(point.x + 1,point.y,point.z),discovered);
-        Fill(new Vector3(point.x - 1,point.y,point.z),discovered);
-        Fill(new Vector3(point.x,point.y,point.z + 1),discovered);
-        Fill(new Vector3(point.x,point.y,point.z - 1),discovered);
+        await Task.Delay(_floodFillAwaitTime);
+        List<Task> tasks = new List<Task>();
+        tasks.Add(Fill(new Vector3(point.x + 1,point.y,point.z),discovered));
+        tasks.Add(Fill(new Vector3(point.x - 1,point.y,point.z),discovered));
+        tasks.Add(Fill(new Vector3(point.x,point.y,point.z + 1),discovered));
+        tasks.Add(Fill(new Vector3(point.x,point.y,point.z - 1),discovered));
+
+        while (!LoadingIsDone(tasks))
+        {
+            await Task.Delay(1);
+        }
         
+        
+        
+        bool LoadingIsDone(List<Task> tasks)
+        {
+            bool isDone = false;
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                isDone = tasks[i].IsCompleted;
+                if (!isDone)
+                {
+                    break;
+                }
+            }
+            return isDone;
+        }
     }
     
-    public void DebugTileMap(bool flip)
+    private async Task SpawnWalls()
+    {
+        for (int y = 1; y < _dungeonTileMap.GetLength(0) - 1; y++)
+        {
+            for (int x = 1; x < _dungeonTileMap.GetLength(1) - 1; x++)
+            {
+                if(_dungeonTileMap[y, x] < 0 || _dungeonTileMap[y - 1, x] < 0 || _dungeonTileMap[y, x - 1] < 0 || _dungeonTileMap[y - 1, x - 1] < 0)
+                    continue;
+                
+                int index = Mathf.Clamp(_dungeonTileMap[y, x], 0, 1) * 2 +
+                            Mathf.Clamp(_dungeonTileMap[y - 1, x], 0, 1) * 1 +
+                            Mathf.Clamp(_dungeonTileMap[y, x - 1], 0, 1) * 4 +
+                            Mathf.Clamp(_dungeonTileMap[y - 1, x - 1], 0, 1) * 8;
+                Vector3 position = new Vector3(x - .5f, _dungeonRoomsTransforms[_dungeonRoomsMap[y, x]].position.y, y - .5f);
+                
+                var tr = Instantiate(_tilesPrefabs[index], position, Quaternion.identity).transform;
+                tr.parent = _dungeonRoomsTransforms[_dungeonRoomsMap[y, x]];
+                tr.name = $"{_tilesPrefabs[index].name}_{x},{y}_{_dungeonTileMap[y, x]} 2,{_dungeonTileMap[y - 1, x]} 1,{_dungeonTileMap[y, x - 1]} 4,{_dungeonTileMap[y - 1, x - 1]} 8";
+                if(_wallDebugMode == WallDebugMode.EachTile)
+                    await Task.Delay(_wallCreationAwaitTime);
+            }
+            if(_wallDebugMode == WallDebugMode.Line)
+                await Task.Delay(_wallCreationAwaitTime);
+        }
+    }
+    
+    public void DebugTileMap(bool flip, bool spawnAssets)
     {
         int rows = _dungeonTileMap.GetLength(0);
         int cols = _dungeonTileMap.GetLength(1);
@@ -137,5 +190,18 @@ public class DungeonFiller : MonoBehaviour
         }
     
         Debug.Log(sb.ToString());
+        
+        if(!spawnAssets)
+            return;
+
+        for (int y = 0; y < _dungeonTileMap.GetLength(0); y++)
+        {
+            for (int x = 0; x < _dungeonTileMap.GetLength(1); x++)
+            {
+                int index = _dungeonTileMap[y, x] < 0 ? 0 : _dungeonTileMap[y, x];
+                Vector3 position = new Vector3(x, 0, y);
+                var tr = Instantiate(_debugPrefabs[index], position, Quaternion.identity).transform;
+            }
+        }
     }
 }
